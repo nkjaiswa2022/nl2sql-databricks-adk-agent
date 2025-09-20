cd3095da7fbb1da43c11e962fb01586555e9e26e
Nilesh Jaiswal <108112053+nkjaiswa2022@users.noreply.github.com>
2025-09-21 03:11:16 +0530

    Create agent.py

---
+++ adk-agent/agent.py
@@ -0,0 +1,50 @@
+import os
+from google.adk.agents import LlmAgent
+from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
+from mcp import StdioServerParameters
+
+
+PATH_TO_YOUR_MCP_SERVER_SCRIPT = os.path.join(os.path.dirname(__file__), "databricks_mcp_server.py")
+if PATH_TO_YOUR_MCP_SERVER_SCRIPT == "/path/to/your/my_adk_mcp_server.py":
+    print("WARNING: PATH_TO_YOUR_MCP_SERVER_SCRIPT is not set. Please update it in agent.py.")
+    # Optionally, raise an error if the path is critical
+PROMPT="""You are a helpful assistant that can answer questions about travel data.
+    You have access to a tool called `execute_databricks_query` which can execute SQL queries against a databricks database.
+
+    The database contains the following tables:
+
+    - `flights`: Contains flight information.
+        - Columns: `flight_id` (INTEGER), `origin` (TEXT), `destination` (TEXT), `departure_time` (TEXT), `arrival_time` (TEXT), `price` (REAL)
+
+    - `hotels`: Contains hotel details.
+        - Columns: `hotel_id` (INTEGER), `name` (TEXT), `location` (TEXT), `price_per_night` (REAL), `rating` (REAL)
+
+    - `bookings`: Records customer bookings for flights and hotels.
+        - Columns: `booking_id` (INTEGER), `customer_name` (TEXT), `type` (TEXT, 'flight' or 'hotel'), `item_id` (INTEGER), `booking_date` (TEXT)
+
+**Step 1 **
+    When a user asks a question about the data, translate the question into a SQL query using the available tables and columns
+** step 2  **
+    - invoke mcp server tool execute_databricks_query with query generated in step 1 as parameter.
+    - output will be list of dictionary , please extract that in the form of a tabular output and return to the user
+    -  If you cannot answer the question using a SQL query based on the available tables, respond that you can only answer questions about the travel data
+     """
+
+root_agent = LlmAgent(
+    model='gemini-2.0-flash',
+    name='databricks_mcp_client_agent',
+    instruction=PROMPT,
+    tools=[
+        McpToolset(
+            connection_params=StdioConnectionParams(
+                server_params = StdioServerParameters(
+                    command='python3', # Command to run your MCP server script
+                    args=[PATH_TO_YOUR_MCP_SERVER_SCRIPT], # Argument is the path to the script
+                ),
+                timeout=60,
+            )
+            
+        )
+    ],
+)
